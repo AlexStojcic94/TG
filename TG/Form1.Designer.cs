6915e11eafc2520a6f0ac9f90423439f177e9036
dev13 <dev13@profitoptics.com>
2018-06-10 19:26:34 +0200

    Enter commit message

--- TG/Form1.Designer.cs
+++ TG/Form1.Designer.cs
@@ -28,71 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.Map = new System.Windows.Forms.PictureBox();
+            this.Distance = new System.Windows.Forms.TrackBar();
+            this.SearchByName = new System.Windows.Forms.TextBox();
+            this.ZoomIn = new System.Windows.Forms.Button();
+            this.ZoomOut = new System.Windows.Forms.Button();
+            this.Search1 = new System.Windows.Forms.CheckBox();
+            this.Search2 = new System.Windows.Forms.CheckBox();
+            this.Search3 = new System.Windows.Forms.CheckBox();
+            this.Search4 = new System.Windows.Forms.CheckBox();
+            this.Search5 = new System.Windows.Forms.CheckBox();
+            this.Search6 = new System.Windows.Forms.CheckBox();
+            this.SearchByType = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Map)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Distance)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // Map
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(68, 67);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(648, 248);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.Map.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Map.Location = new System.Drawing.Point(12, 38);
+            this.Map.Name = "Map";
+            this.Map.Size = new System.Drawing.Size(802, 362);
+            this.Map.TabIndex = 0;
+            this.Map.TabStop = false;
+            this.Map.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Map_MouseClick);
             // 
-            // button1
+            // Distance
             // 
-            this.button1.Location = new System.Drawing.Point(109, 13);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Distance.Location = new System.Drawing.Point(12, 419);
+            this.Distance.Maximum = 100;
+            this.Distance.Name = "Distance";
+            this.Distance.Size = new System.Drawing.Size(802, 45);
+            this.Distance.TabIndex = 1;
+            this.Distance.Value = 2;
             // 
-            // button2
+            // SearchByName
             // 
-            this.button2.Location = new System.Drawing.Point(362, 13);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.SearchByName.Location = new System.Drawing.Point(623, 12);
+            this.SearchByName.Name = "SearchByName";
+            this.SearchByName.Size = new System.Drawing.Size(191, 20);
+            this.SearchByName.TabIndex = 2;
             // 
-            // button3
+            // ZoomIn
             // 
-            this.button3.Location = new System.Drawing.Point(600, 13);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.ZoomIn.Location = new System.Drawing.Point(769, 308);
+            this.ZoomIn.Name = "ZoomIn";
+            this.ZoomIn.Size = new System.Drawing.Size(33, 33);
+            this.ZoomIn.TabIndex = 3;
+            this.ZoomIn.Text = "+";
+            this.ZoomIn.UseVisualStyleBackColor = true;
+            this.ZoomIn.Click += new System.EventHandler(this.ZoomIn_Click);
+            // 
+            // ZoomOut
+            // 
+            this.ZoomOut.Location = new System.Drawing.Point(769, 347);
+            this.ZoomOut.Name = "ZoomOut";
+            this.ZoomOut.Size = new System.Drawing.Size(33, 33);
+            this.ZoomOut.TabIndex = 4;
+            this.ZoomOut.Text = "-";
+            this.ZoomOut.UseVisualStyleBackColor = true;
+            this.ZoomOut.Click += new System.EventHandler(this.ZoomOut_Click);
+            // 
+            // Search1
+            // 
+            this.Search1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.Search1.AutoSize = true;
+            this.Search1.Location = new System.Drawing.Point(20, 511);
+            this.Search1.Name = "Search1";
+            this.Search1.Size = new System.Drawing.Size(71, 23);
+            this.Search1.TabIndex = 5;
+            this.Search1.Text = "checkBox1";
+            this.Search1.UseVisualStyleBackColor = true;
+            // 
+            // Search2
+            // 
+            this.Search2.Appearance = System.Windows.Forms.Appearance.Button;
+            this.Search2.AutoSize = true;
+            this.Search2.Location = new System.Drawing.Point(156, 511);
+            this.Search2.Name = "Search2";
+            this.Search2.Size = new System.Drawing.Size(71, 23);
+            this.Search2.TabIndex = 6;
+            this.Search2.Text = "checkBox2";
+            this.Search2.UseVisualStyleBackColor = true;
+            // 
+            // Search3
+            // 
+            this.Search3.Appearance = System.Windows.Forms.Appearance.Button;
+            this.Search3.AutoSize = true;
+            this.Search3.Location = new System.Drawing.Point(299, 511);
+            this.Search3.Name = "Search3";
+            this.Search3.Size = new System.Drawing.Size(71, 23);
+            this.Search3.TabIndex = 7;
+            this.Search3.Text = "checkBox3";
+            this.Search3.UseVisualStyleBackColor = true;
+            // 
+            // Search4
+            // 
+            this.Search4.Appearance = System.Windows.Forms.Appearance.Button;
+            this.Search4.AutoSize = true;
+            this.Search4.Location = new System.Drawing.Point(420, 511);
+            this.Search4.Name = "Search4";
+            this.Search4.Size = new System.Drawing.Size(71, 23);
+            this.Search4.TabIndex = 8;
+            this.Search4.Text = "checkBox4";
+            this.Search4.UseVisualStyleBackColor = true;
+            // 
+            // Search5
+            // 
+            this.Search5.Appearance = System.Windows.Forms.Appearance.Button;
+            this.Search5.AutoSize = true;
+            this.Search5.Location = new System.Drawing.Point(563, 511);
+            this.Search5.Name = "Search5";
+            this.Search5.Size = new System.Drawing.Size(71, 23);
+            this.Search5.TabIndex = 9;
+            this.Search5.Text = "checkBox5";
+            this.Search5.UseVisualStyleBackColor = true;
+            // 
+            // Search6
+            // 
+            this.Search6.Appearance = System.Windows.Forms.Appearance.Button;
+            this.Search6.AutoSize = true;
+            this.Search6.Location = new System.Drawing.Point(691, 511);
+            this.Search6.Name = "Search6";
+            this.Search6.Size = new System.Drawing.Size(71, 23);
+            this.Search6.TabIndex = 10;
+            this.Search6.Text = "checkBox6";
+            this.Search6.UseVisualStyleBackColor = true;
+            // 
+            // SearchByType
+            // 
+            this.SearchByType.Location = new System.Drawing.Point(20, 470);
+            this.SearchByType.Name = "SearchByType";
+            this.SearchByType.Size = new System.Drawing.Size(143, 20);
+            this.SearchByType.TabIndex = 11;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(826, 567);
+            this.Controls.Add(this.SearchByType);
+            this.Controls.Add(this.Search6);
+            this.Controls.Add(this.Search5);
+            this.Controls.Add(this.Search4);
+            this.Controls.Add(this.Search3);
+            this.Controls.Add(this.Search2);
+            this.Controls.Add(this.Search1);
+            this.Controls.Add(this.ZoomOut);
+            this.Controls.Add(this.ZoomIn);
+            this.Controls.Add(this.SearchByName);
+            this.Controls.Add(this.Distance);
+            this.Controls.Add(this.Map);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Map)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Distance)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox Map;
+        private System.Windows.Forms.TrackBar Distance;
+        private System.Windows.Forms.TextBox SearchByName;
+        private System.Windows.Forms.Button ZoomIn;
+        private System.Windows.Forms.Button ZoomOut;
+        private System.Windows.Forms.CheckBox Search1;
+        private System.Windows.Forms.CheckBox Search2;
+        private System.Windows.Forms.CheckBox Search3;
+        private System.Windows.Forms.CheckBox Search4;
+        private System.Windows.Forms.CheckBox Search5;
+        private System.Windows.Forms.CheckBox Search6;
+        private System.Windows.Forms.TextBox SearchByType;
     }
 }
 
--- TG/Form1.cs
+++ TG/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -14,28 +15,61 @@ namespace TG
     {
         SharpMap.Map _sharpMap;
         const float ZOOM_FACTOR = 0.3f;
-        const String DATA_NAME = "ROADS";
-        const String DATA_PATH = @"C:\Users\alexstojcic\source\repos\TG\TG\bin\Debug\world_adm0\roads.shp";
+        int zoom_level = 1;
+        string connString = "Server=127.0.0.1;Port=5432;User Id=postgres;Password=admin;Database=TG;";
+        string roadsTableName = "roads";
+        string geomname = "geom";
+        string idname = "gid";
 
         public Form1()
         {
             InitializeComponent();
-            _sharpMap = new SharpMap.Map(new Size(648, 248));
+            _sharpMap = new SharpMap.Map(new Size(802, 362));
             _sharpMap.BackColor = Color.White;
 
-            SharpMap.Layers.VectorLayer roadsLayer = new SharpMap.Layers.VectorLayer(DATA_NAME);
-            roadsLayer.DataSource = new SharpMap.Data.Providers.ShapeFile(DATA_PATH);
 
+            SharpMap.Layers.VectorLayer roadsLayer = new SharpMap.Layers.VectorLayer("Roads");
+            roadsLayer.DataSource = new SharpMap.Data.Providers.PostGIS(connString, roadsTableName, geomname, idname);
+            
             _sharpMap.Layers.Add(roadsLayer);
 
             _sharpMap.ZoomToExtents();
 
-            pictureBox1.Image = _sharpMap.GetMap();
+            Map.Image = _sharpMap.GetMap();
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        
+
+        private void ZoomIn_Click(object sender, EventArgs e)
         {
+            if (zoom_level < 6)
+            {
+                zoom_level += 1;
+                _sharpMap.Zoom = _sharpMap.Zoom * ZOOM_FACTOR;
+                Map.Image = _sharpMap.GetMap();
+            }
+        }
+
+        private void ZoomOut_Click(object sender, EventArgs e)
+        {
+            if (zoom_level > 1)
+            {
+                zoom_level--;
+                _sharpMap.Zoom = _sharpMap.Zoom / ZOOM_FACTOR;
+                Map.Image = _sharpMap.GetMap();
+            }
+        }
+
+        
+
+        private void Map_MouseClick(object sender, MouseEventArgs e)
+        {
+            GeoAPI.Geometries.Coordinate p = _sharpMap.ImageToWorld(new PointF(e.X, e.Y));
+            _sharpMap.Center.X = p.X;
+            _sharpMap.Center.Y = p.Y;
+
+            Map.Image = _sharpMap.GetMap();
         }
     }
 }
